60894d9b5ff9808567b3b058345569016058906d
Paul Irwin <irwinj@southern.edu>
2021-07-08 16:17:13 -0400

    Added loading js interop methods.

---
+++ DotNetLibrary.Core/System/Security/Claims/SimpleClaim.cs
@@ -0,0 +1,9 @@
+﻿namespace System.Security.Claims
+{
+	public struct SimpleClaim
+	{
+		public string Type { get; set; }
+		public string Value { get; set; }
+		public string Issuer { get; set; }
+	}
+}
