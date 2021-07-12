fc73618eed66e52d68c41f03dd71e1c9e744a669
Paul <daemogar@gmail.com>
2021-07-12 15:55:08 -0400

    Added cookies extensions method.

---
+++ DotNetLibrary.Server/Microsoft/Extensions/DependencyInjection/CookieExtensions.cs
@@ -0,0 +1,13 @@
+﻿using DotNetLibrary.Cookies;
+
+namespace Microsoft.Extensions.DependencyInjection
+{ 
+	public static class CookieExtensions
+	{
+		public static void AddCookies(this IServiceCollection services)
+		{
+			services.AddTransient<CookieManager>();
+			services.AddTransient<ImpersonationCookie>();
+		}
+	}
+}
