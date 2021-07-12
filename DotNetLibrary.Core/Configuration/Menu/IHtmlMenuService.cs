7d93210e1c236bd361fc145805be9c1120723221
Paul <daemogar@gmail.com>
2021-07-12 17:02:43 -0400

    Added basic html menu handler.

---
+++ DotNetLibrary.Core/Configuration/Menu/IHtmlMenuService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using System;
+
+namespace DotNetLibrary.Configuration.Menu
+{
+	public interface IHtmlMenuService
+	{
+		bool HasMenu { get; }
+		MarkupString Markup { get; }
+		event Action StateChanged;
+	}
+}
