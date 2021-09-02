556bed90e2370215a590280c58ec2f9eab264005
Paul Irwin <daemogar@gmail.com>
2021-09-02 10:16:28 -0400

    Updated nuget packages. Removed AutoMoq because it does not support net6.

---
+++ DotNetLibrary.Tests/Client/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+
+using Xunit;
+
+namespace DotNetLibrary.Tests
+{
+	public class ClientUnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+			//Example e = new();
+			//Microsoft.Extensions.DependencyInjection.
+			//	AppSettingsExtensions.AddSettings(null, null);
+
+
+		}
+	}
+}
---
+++ DotNetLibrary.Tests/Server/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+
+using Xunit;
+
+namespace DotNetLibrary.Tests
+{
+	public class ServerUnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+			
+			//Example e = new();
+			//Microsoft.Extensions.DependencyInjection.
+			//	AppSettingsExtensions.AddSettings(null, null);
+
+
+		}
+	}
+}
