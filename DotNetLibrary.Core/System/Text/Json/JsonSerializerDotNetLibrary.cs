227102b2414ba910c678a2d510e2c5c87833c686
irwinj <irwinj@southern.edu>
2022-02-24 12:05:08 -0500

    Enhanced custom json serializer options.

--- DotNetLibrary.Core/System/Text/Json/JsonSerializerDotNetLibrary.cs
+++ DotNetLibrary.Core/System/Text/Json/JsonSerializerDotNetLibrary.cs
@@ -1,15 +1,11 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Text.Json.Serialization;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace System.Text.Json;
 
 /// <summary>
 /// Json serialization and deserialization helper extension methods.
 /// </summary>
-public static class JsonExtensions
+public static class JsonSerializerDotNetLibrary
 {
 	private static JsonSerializerOptions Options { get; } = new()
 	{
@@ -25,13 +21,35 @@ public static class JsonExtensions
 	};
 
 	/// <summary>
+	/// Get the default options configuration object used for calls that 
+	/// do not supply an options object or option callback method.
+	/// </summary>
+	/// <returns>The default configuration.</returns>
+	public static JsonSerializerOptions DefaultOptions()
+		=> DefaultOptions(p => { });
+
+	/// <summary>
+	/// Get the default options configuration object and mutate it with
+	/// the <paramref name="optionsCallback"/>.
+	/// </summary>
+	/// <param name="optionsCallback">Callback to mutate the default options before passing it to the Serializer or Deserializer.</param>
+	/// <returns>The mutated default configuration.</returns>
+	public static JsonSerializerOptions DefaultOptions(
+		Action<JsonSerializerOptions> optionsCallback)
+	{
+		JsonSerializerOptions options = new(Options);
+		optionsCallback(options);
+		return options;
+	}
+
+	/// <summary>
 	/// Convert target object into a JSON serialized string.
 	/// </summary>
 	/// <typeparam name="T">Any object type can be converted.</typeparam>
 	/// <param name="data">Object that should be serialized to a json string.</param>
 	/// <returns>JSON string of the object.</returns>
 	public static string ToJsonString<T>(this T data)
-		=> data.ToJsonString(Options);
+			=> data.ToJsonString(Options);
 
 	/// <summary>
 	/// Convert target object into a JSON serialized string.
@@ -41,10 +59,10 @@ public static class JsonExtensions
 	/// <param name="writeIndented">Should it be formatted with tabs and returns.</param>
 	/// <returns>JSON string of the object.</returns>
 	public static string ToJsonString<T>(this T data, bool writeIndented = false)
-		=> JsonSerializer.Serialize(data, new JsonSerializerOptions(Options)
+		=> JsonSerializer.Serialize(data, DefaultOptions(p =>
 		{
-			WriteIndented = writeIndented,
-		});
+			p.WriteIndented = writeIndented;
+		}));
 
 	/// <summary>
 	/// Convert target object into a JSON serialized string.
@@ -57,13 +75,24 @@ public static class JsonExtensions
 		=> JsonSerializer.Serialize(data, options);
 
 	/// <summary>
+	/// Convert target object into a JSON serialized string.
+	/// </summary>
+	/// <typeparam name="T">Any object type can be converted.</typeparam>
+	/// <param name="data">Object that should be serialized to a json string.</param>
+	/// <param name="optionsCallback">Callback to configure json serializer options.</param>
+	/// <returns>JSON string of the object.</returns>
+	public static string ToJsonString<T>(this T data, Action<JsonSerializerOptions> optionsCallback)
+		=> JsonSerializer.Serialize(data, DefaultOptions(optionsCallback));
+
+	/// <summary>
 	/// Convert a json string into an object.
 	/// </summary>
 	/// <typeparam name="T">Any object type can be converted.</typeparam>
 	/// <param name="json">A string json representation to be converted into an object.</param>
+	/// <param name="options">Options for <seealso cref="JsonSerializer.Deserialize(string, Type, JsonSerializerOptions?)"/>.</param>
 	/// <returns>The object that is returned from deserializing the json string.</returns>
-	public static T FromJsonString<T>(this string json)
-		=> (T)json.FromJsonStringUsingType(typeof(T));
+	public static T FromJsonString<T>(this string json, JsonSerializerOptions? options = null)
+			=> (T)json.FromJsonStringUsingType(typeof(T), options);
 
 	/// <summary>
 	/// Convert a json string into an object.
@@ -71,21 +100,23 @@ public static class JsonExtensions
 	/// <typeparam name="T">Any object type can be converted.</typeparam>
 	/// <param name="json">A string json representation to be converted into an object.</param>
 	/// <param name="model">A target type to convert the json string into.</param>
+	/// <param name="options"><inheritdoc cref="ToJsonString{T}(T)"/></param>
 	/// <returns>The object that is returned from deserializing the json string.</returns>
-	public static T FromJsonStringUsingModel<T>(this string json, T model)
-		=> (T)json.FromJsonStringUsingType(model?.GetType() ?? typeof(T));
+	public static T FromJsonStringUsingModel<T>(this string json, T model, JsonSerializerOptions? options = null)
+			=> (T)json.FromJsonStringUsingType(model?.GetType() ?? typeof(T), options);
 
 	/// <summary>
 	/// Convert a json string into an object.
 	/// </summary>
 	/// <param name="json">A string json representation to be converted into an object.</param>
 	/// <param name="type">A target type to convert the json string into.</param>
+	/// <param name="options"><inheritdoc cref="ToJsonString{T}(T)"/></param>
 	/// <returns>The object that is returned from deserializing the json string.</returns>
-	public static object FromJsonStringUsingType(this string json, Type type)
+	public static object FromJsonStringUsingType(this string json, Type type, JsonSerializerOptions? options = null)
 	{
 		try
 		{
-			return JsonSerializer.Deserialize(json, type, Options)!;
+			return JsonSerializer.Deserialize(json, type, options ?? Options)!;
 		}
 		catch (JsonException e)
 		{
