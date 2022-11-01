c5881d3b01f38b3a5e3a271c8f65b8ca6c1dafcb
irwinj <irwinj@southern.edu>
2022-11-01 18:05:40 -0400

    Fixed inputs for the basic cookie.

--- DotNetLibrary.Core/Microsoft/AspNetCore/Http/BasicCookie.cs
+++ DotNetLibrary.Core/Microsoft/AspNetCore/Http/BasicCookie.cs
@@ -6,9 +6,7 @@ namespace Microsoft.AspNetCore.Http;
 /// </summary>
 public record class BasicCookie<T>: IBasicCookie
 {
-	/// <summary>
-	/// The underlying Cookie Manager.
-	/// </summary>
+	/// <inheritdoc cref="IBasicCookieManager" />
 	protected IBasicCookieManager Manager { get; }
 
 	/// <summary>
@@ -16,51 +14,45 @@ public record class BasicCookie<T>: IBasicCookie
 	/// </summary>
 	public string CookieKey { get; }
 
+	private CookieOptions? Options { get; }
+
 	/// <summary>
 	/// Constructor that takes a manager and cookie key.
 	/// </summary>
 	/// <param name="manager">A cookie manager for accessing the underlying cookies.</param>
 	/// <param name="cookieKey">The key of the cookie.</param>
-	public BasicCookie(IBasicCookieManager manager, string cookieKey)
+	/// <param name="options"><seealso cref="CookieOptions" /></param>
+	public BasicCookie(
+		IBasicCookieManager manager,
+		string cookieKey,
+		CookieOptions? options = default!)
 	{
 		Debug.Assert(manager is not null);
 		Debug.Assert(cookieKey is not null);
 
 		Manager = manager;
 		CookieKey = cookieKey!;
+		Options = options;
 	}
 
 	/// <summary>
 	/// Get cookie from cookie manager using the constructor cookie key.
 	/// </summary>
+	/// <param name="allowNull">If a null value is allowed or not.</param>
 	/// <returns>The string value of the cookie.</returns>
-	public async Task<string> GetAsync()
-		=> await Manager.GetRequestCookieAsync(CookieKey);
+	public async Task<string> GetAsync(bool allowNull = false)
+		=> await Manager.GetRequestCookieAsync(CookieKey, allowNull, Options!);
 
 	/// <summary>
 	/// Set the cookie value using the manager and the constructor cookie key.
 	/// </summary>
 	/// <param name="value">Cookie value.</param>
 	public async Task SetAsync(string value)
-		=> await SetAsync(value, new());
-
-	/// <summary>
-	/// Set the cookie value using the manager and the constructor cookie key.
-	/// </summary>
-	/// <param name="value">Cookie value.</param>
-	/// <param name="options">CookieOptions</param>
-	public async Task SetAsync(string value, CookieOptions options)
-		=> await Manager.AppendResponseCookieAsync(CookieKey, value, options);
+		=> await Manager.AppendResponseCookieAsync(CookieKey, value, Options!);
 
 	/// <summary>
 	/// Delete cookie using manager and using the constructor cookie key.
 	/// </summary>
-	public async Task DeleteAsync() => await DeleteAsync(new());
-
-	/// <summary>
-	/// Delete cookie using manager and using the constructor cookie key.
-	/// </summary>
-	/// <param name="options">CookieOptions</param>
-	public async Task DeleteAsync(CookieOptions options)
-		=> await Manager.DeleteCookieAsync(CookieKey, options);
+	public async Task DeleteAsync()
+		=> await Manager.DeleteCookieAsync(CookieKey, Options!);
 }
--- DotNetLibrary.Core/Microsoft/AspNetCore/Http/IBasicCookie.cs
+++ DotNetLibrary.Core/Microsoft/AspNetCore/Http/IBasicCookie.cs
@@ -3,9 +3,7 @@
 public interface IBasicCookie
 {
 	string CookieKey { get; }
-	Task<string> GetAsync();
+	Task<string> GetAsync(bool allowNull = false);
 	Task SetAsync(string value);
-	Task SetAsync(string value, CookieOptions options);
 	Task DeleteAsync();
-	Task DeleteAsync(CookieOptions options);
 }
