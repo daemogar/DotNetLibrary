61e2aaf652fdafded72e75d01582ae8ad0850555
irwinj <irwinj@southern.edu>
2023-01-30 15:16:45 -0500

    Added null cookie documentation.

--- DotNetLibrary.Core/Microsoft/AspNetCore/Http/NullCookieValueException.cs
+++ DotNetLibrary.Core/Microsoft/AspNetCore/Http/NullCookieValueException.cs
@@ -1,9 +1,19 @@
 ﻿namespace Microsoft.AspNetCore.Http;
 
+/// <summary>
+/// Exception throw when a cookie has a null value and it is not allowed.
+/// </summary>
 public class NullCookieValueException : Exception
 {
+	/// <summary>
+	/// The cookie key that has the null value.
+	/// </summary>
 	public string CookieKey { get; }
 
+	/// <summary>
+	/// Create a null cookie exception for the given <paramref name="cookieKey"/>
+	/// </summary>
+	/// <param name="cookieKey">The cookie key that has the null value.</param>
 	public NullCookieValueException(string cookieKey)
 		: base($"The {cookieKey} cookie was null, and null for this cookie is not allowed.")
 	{
